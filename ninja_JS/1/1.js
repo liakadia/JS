7cd9ef5547ad7fc99b1aa1febe6c9a0ff5ba39eb
liakadia <konstantin@MacBook-Air-konstantin.local>
2017-04-30 03:32:27 +0300

    удобно вывод

--- ninja_JS/1/1.js
+++ ninja_JS/1/1.js
@@ -1,2 +1,10 @@
 //'use strict' использовать последние версии js 
-alert(1);
+
+// преобрадования
+var test = 1;
+var test1= "1";
+
+document.write("test=",test," тип=", typeof test ," test1=", test1," тип=" , typeof test1,"<br>");
+
+document.write("test=",+test," тип=", typeof +test ," test1=", +test1," тип=" , typeof +test1,"<br>");//преобразует в число
+document.write("test=",-test," тип=", typeof -test ," test1=", -test1," тип=" , typeof -test1,"<br>");// делает противоположный знак
