d2377490fb11042513a7149128b3198ecbd827e1
liakadia <konstantin@MacBook-Air-konstantin.local>
2017-05-13 21:34:54 +0300

    продолжение 2 урока

--- ninja_JS/2/1.js
+++ ninja_JS/2/1.js
@@ -16,8 +16,44 @@ document.write("bitrhday=",user['bitrhday'],"<br>");
 
 //или такое обьявление
 var test ={
-	name:'name'
+	name:'name',
+	age: null
 };
+//массивы
+ var mass=[1,2,3,4,5];
+
+//длинна массива- индекс последнего +1 
+document.write(mass.length,"<br>");
+
+//все числовые типы - number
+if(typeof mass[0]=='number'){
+document.write('выводим если переменная типа number: ',mass[0],'<br>');}
+else{
+document.write('error');
+}
+// switch 
+var kostia="tralala";
+switch(kostia){
+	case'trall': document.write('ne'); 	
+		case'trala': document.write('ke'); break;
+			case'tralala': document.write('fe'); break;
+	default: document.write('ma'); 
+}
+
+document.write("<br><br>");
+var i=0;
+while( i< mass.length){
+	document.write(mass[i]);
+	i++;
+}
+i=0;
+document.write("<br><br>");
 
 
+do{
+document.write(mass[mass.length-i-1]);
+	i++;
+}while(i<mass.length);
 
+
+document.write("<br><br>");
